acd4a9d985a068ec39609897b5f61628c3d6f0e6
Олег Мангол <staspovetko166@gmail.com>
2025-10-20 11:18:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ def guess_number():
     print("Я загадал число от 1 до 100. Попробуйте угадать!")
     #Создаю цикл
  while True:
+     try:
+            guess = int(input("Ваш ответ: "))
+            attempts += 1
+            if guess < number:
+                print("Слишком мало!")
+            elif guess > number:
+                print("Слишком много!")
+            else:
+                print(f"Поздравляю! Вы угадали число за {attempts} попыток.")
+                break
